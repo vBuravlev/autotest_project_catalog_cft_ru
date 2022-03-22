7cdb22c154f7a3948ed0ca058981d40fa0807007
Vadim Buravlev <masta54@mail.ru>
2022-03-22 23:33:56 +0700

    fix16

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -5,8 +5,8 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/remote.properties",
-        "classpath:config/local.properties"
+        "classpath:config/local.properties",
+        "classpath:config/remote.properties"
 })
 public interface ProjectConfig extends Config {
 
@@ -19,6 +19,5 @@ public interface ProjectConfig extends Config {
     String browserMobileView();
     String remoteDriverUrl();
     String videoStorage();
-    @DefaultValue("4000")
     int timeout();
 }
