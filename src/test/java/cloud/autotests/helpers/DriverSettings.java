ac060fb30d8bcfda8939c9ecf7224fd86d751b48
Vadim Buravlev <masta54@mail.ru>
2022-03-23 21:45:39 +0700

    fix28

--- src/test/java/cloud/autotests/helpers/DriverSettings.java
+++ src/test/java/cloud/autotests/helpers/DriverSettings.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.HashMap;
@@ -20,29 +21,31 @@ public class DriverSettings {
         Configuration.timeout=Project.config.timeout();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        OperaOptions operaOptions = new OperaOptions();
 
         if ((Project.config.browser()).equals("chrome")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--no-sandbox");
             chromeOptions.addArguments("--disable-infobars");
             chromeOptions.addArguments("--disable-popup-blocking");
             chromeOptions.addArguments("--disable-notifications");
             chromeOptions.addArguments("--lang=en-en");
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            if (Project.isWebMobile()) { // for chrome only
+                Map<String, Object> mobileDevice = new HashMap<>();
+                mobileDevice.put("deviceName", Project.config.browserMobileView());
+                chromeOptions.setExperimentalOption("mobileEmulation", mobileDevice);
+            }
         } else if ((Project.config.browser()).equals("firefox")) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
             firefoxOptions.addArguments("--fast-start");
             firefoxOptions.addArguments("--enable-logging");
             firefoxOptions.addArguments("--ignore-certificate-errors");
             firefoxOptions.addArguments("--disable-gpu");
+            capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS , firefoxOptions);
         } else if ((Project.config.browser()).equals("opera")) {
+            OperaOptions operaOptions = new OperaOptions();
             operaOptions.addArguments("--disable-gpu");
-        }
-
-            if (Project.isWebMobile()) { // for chrome only
-            Map<String, Object> mobileDevice = new HashMap<>();
-            mobileDevice.put("deviceName", Project.config.browserMobileView());
-            chromeOptions.setExperimentalOption("mobileEmulation", mobileDevice);
+            capabilities.setCapability(OperaOptions.CAPABILITY, operaOptions);
         }
 
         if (Project.isRemoteWebDriver()) {
@@ -50,8 +53,7 @@ public class DriverSettings {
             capabilities.setCapability("enableVideo", true);
             Configuration.remote = Project.config.remoteDriverUrl();
         }
-
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         Configuration.browserCapabilities = capabilities;
+
     }
 }
